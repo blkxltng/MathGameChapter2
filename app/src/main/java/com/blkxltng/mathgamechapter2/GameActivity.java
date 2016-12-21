c0e2a4945f021bcf54257b6b9d9ee33d8c275068
blkxltng <firejama@gmail.com>
2016-12-21 14:48:57 -0500

    Added GameActivity

---
+++ app/src/main/java/com/blkxltng/mathgamechapter2/GameActivity.java
@@ -0,0 +1,86 @@
+package com.blkxltng.mathgamechapter2;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class GameActivity extends AppCompatActivity implements View.OnClickListener {
+
+    int correctAnswer;
+    Button buttonObjectChoice1;
+    Button buttonObjectChoice2;
+    Button buttonObjectChoice3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+
+        //Initiate variables
+        int partA = 9;
+        int partB = 9;
+        correctAnswer = partA * partB;
+        int wrongAnswer1 = correctAnswer - 1;
+        int wrongAnswer2 = correctAnswer + 1;
+
+        //Bindings
+        TextView textObjectPartA = (TextView) findViewById(R.id.textPartA);
+        TextView textObjectPartB = (TextView) findViewById(R.id.textPartB);
+        buttonObjectChoice1 = (Button) findViewById(R.id.buttonChoice1);
+        buttonObjectChoice2 = (Button) findViewById(R.id.buttonChoice2);
+        buttonObjectChoice3 = (Button) findViewById(R.id.buttonChoice3);
+
+        //Set the textviews
+        textObjectPartA.setText("" + partA);
+        textObjectPartB. setText("" + partB);
+
+        //Set the buttons
+        buttonObjectChoice1.setText("" + wrongAnswer1);
+        buttonObjectChoice2.setText("" + correctAnswer);
+        buttonObjectChoice3.setText("" + wrongAnswer2);
+
+        //Set the listeners
+        buttonObjectChoice1.setOnClickListener(this);
+        buttonObjectChoice2.setOnClickListener(this);
+        buttonObjectChoice3.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        int answerGiven = 0;
+
+        switch (view.getId()) {
+
+            case R.id.buttonChoice1:
+                answerGiven = Integer.parseInt("" + buttonObjectChoice1.getText());
+                if(answerGiven == correctAnswer) {
+                    Toast.makeText(getApplicationContext(), "Congrats!", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Incorrect", Toast.LENGTH_SHORT).show();
+                }
+                break;
+
+            case R.id.buttonChoice2:
+                answerGiven = Integer.parseInt("" + buttonObjectChoice2.getText());
+                if(answerGiven == correctAnswer) {
+                    Toast.makeText(getApplicationContext(), "Congrats!", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Incorrect", Toast.LENGTH_SHORT).show();
+                }
+                break;
+
+            case R.id.buttonChoice3:
+                answerGiven = Integer.parseInt("" + buttonObjectChoice3.getText());
+                if(answerGiven == correctAnswer) {
+                    Toast.makeText(getApplicationContext(), "Congrats!", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Incorrect", Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
+    }
+}
